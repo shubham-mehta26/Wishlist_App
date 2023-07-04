1bf91aadd23206e226fbeaa3e067b9744e3172f3
Shubham Mehta <shubham.sm911@gmail.com>
2023-07-04 15:15:12 +0530

    ui made better

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const wishlistInputElement = document.querySelector(".input");
 const form = document.querySelector(".form");
-
+const bodyFullEle = document.querySelector('.edit-button');
 const parentWishlistContainerEle = document.querySelector(".wishlist-container");
+const scoreEle = document.querySelector('.score');
 
 let wishlistValue = "";
 let wishList =[];
@@ -11,14 +12,35 @@ wishlistInputElement.addEventListener('input' , (event)=>{ //input or keyup
     wishlistValue = event.target.value;
 })
 
+const updateScore =()=>{
+  
+  scoreEle.innerHTML=`<span>Total items in wishlist <span/> : ${count}`;
+}
+
+
+const updateCount =()=>{
+  if(wishList){
+    count = wishList.length;
+    console.log(count);
+  }
+  if(count === 0){
+    const emptyMessage = document.createElement('p');
+    emptyMessage.innerHTML = '<p>The Wishlist is empty...<p/><p>Add activities to your bucket List...<p/>';
+    emptyMessage.classList.add('empty-wishlist');
+    parentWishlistContainerEle.appendChild(emptyMessage);
+  }
+}
+// for the time when page reloads
+updateCount();
+
 form.addEventListener('submit' , (event)=>{
     event.preventDefault();
-    count++;
     wishList = [...wishList , wishlistValue];
     wishlistInputElement.value="";
     parentWishlistContainerEle.innerHTML = '';
+    updateCount();
+    updateScore();
     showWishlist(wishList);
-    console.log(count);
 })
 
 const showWishlist = (wishList)=>{
@@ -36,9 +58,15 @@ const showWishlist = (wishList)=>{
         containerEle.appendChild(wishlistTextEle);
 
         const deleteBtnEle = document.createElement('button');
-        deleteBtnEle.innerText = "Delete";
+        deleteBtnEle.className="delete-button";
+        // deleteBtnEle.innerText = "Delete";
         deleteBtnEle.setAttribute('name', wish);
         containerEle.appendChild(deleteBtnEle);
+
+        const deleteIconImg = document.createElement('img');
+        deleteIconImg.setAttribute('src', './delete.png');
+        deleteIconImg.setAttribute('alt', 'Delete');
+        deleteBtnEle.appendChild(deleteIconImg);
 
         parentWishlistContainerEle.appendChild(containerEle);
         
@@ -58,12 +86,38 @@ parentWishlistContainerEle.addEventListener('click' , (event)=>{
     
     
   }
-
+  
   // Check if the clicked element is a delete button
-  if (target.tagName === 'BUTTON' && target.name) {
+  if ((target.tagName === 'BUTTON' || target.tagName=== 'IMG') && target.name) {
     const itemToDelete = target.name;
     wishList = wishList.filter(item => item !== itemToDelete);
     parentWishlistContainerEle.innerHTML = '';
     showWishlist(wishList);
+    updateCount();
+    updateScore();
+  }
+
+  // Check if the clicked element is a delete image
+  if (target.tagName === 'IMG' && target.parentNode.classList.contains('delete-button')) {
+    const deleteBtnEle = target.parentNode;
+    const itemToDelete = deleteBtnEle.getAttribute('name');
+    wishList = wishList.filter((item) => item !== itemToDelete);
+    parentWishlistContainerEle.innerHTML = '';
+    showWishlist(wishList);
+    updateCount();
+    updateScore();
+  }
+
+})
+
+bodyFullEle.addEventListener('click' , (event)=>{
+  // console.log(event);
+  let name = prompt("What is your name?");
+  const namechange = document.querySelector('.message');
+  if(name===null || name.length==0){
+    namechange.innerHTML = `Welcome Guest!`;
+  }
+  else{
+    namechange.innerHTML = `Welcome <i>${name}<i/>`;
   }
 })
